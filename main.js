4b982e717c581e02efa188fae47eee40e610d31a
Ryan Miller <ryandmiller3521@gmail.com>
2021-02-19 09:44:59 -0700

    Add put favorited messages into an array

--- main.js
+++ main.js
@@ -2,10 +2,14 @@ var receiveButton = document.querySelector("#getMessage");
 var meditateImg = document.querySelector(".meditation-img");
 var messageBox = document.querySelector(".message");
 var message = document.querySelector(".message-font");
+var heart = document.querySelector(".favButton");
 var selection = document.forms.selection;
 var radios = selection.elements.selection;
 
+var favoritedMessages = [];
+
 receiveButton.addEventListener("click", showMessage);
+heart.addEventListener("click", favoriteMessage);
 
 function showMessage() {
   hideImage();
@@ -19,17 +23,22 @@ function showMessage() {
   }
 }
 
-function hideImage(){
+function hideImage() {
   meditateImg.classList.add("hidden");
   messageBox.classList.remove("hidden");
 }
 
-function getRandomIndex(array){
+function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
 
 function selectMessage(type){
   var selected = type[getRandomIndex(type)];
-  console.log(selected);
   return selected;
+}
+
+function favoriteMessage() {
+  heart.src = "./assets/pink-heart.jpg"
+  favoritedMessages.push(message.innerText);
+  console.log(favoritedMessages);
 }
