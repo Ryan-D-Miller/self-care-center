fa4ee96bbf7204de93d549ca00141acc2cb734a5
Ryan Miller <ryandmiller3521@gmail.com>
2021-02-20 10:16:41 -0700

    Add remove favorite feature

--- main.js
+++ main.js
@@ -17,6 +17,17 @@ receiveButton.addEventListener("click", showMessage);
 heart.addEventListener("click", favoriteMessage);
 viewFavoriteButton.addEventListener("click", viewFavoritePage);
 backToMainButton.addEventListener("click", switchPages);
+document.addEventListener("click", function(event){
+  checkIfHeart(event);
+})
+
+function checkIfHeart(event){
+  if(event.target && event.target.classList.contains("favPage")){
+    removeFavorite(event.target.parentElement.innerText);
+    addContent();
+  }
+}
+
 
 function showMessage() {
   hideImage();
@@ -54,20 +65,43 @@ function selectMessage(type){
 }
 
 function favoriteMessage() {
-  heart.src = "./assets/pink-heart.jpg"
-  favoritedMessages.push(message.innerText);
+  var checkFavorited = checkIfFavorited();
+  if(checkFavorited){
+    heart.src = "./assets/clear-heart.png";
+    removeFavorite(message.innerText);
+  }else{
+    heart.src = "./assets/pink-heart.jpg"
+    favoritedMessages.push(message.innerText);
+  }
+}
+
+function removeFavorite(removedMessage) {
+  var index = favoritedMessages.indexOf(removedMessage);
+  if(index > -1){
+    favoritedMessages.splice(index, 1);
+  }
+}
+
+function checkIfFavorited() {
+  if(heart.src === "file:///Users/ryanmiller/turing/self-care-center/assets/pink-heart.jpg"){
+    return true;
+  }else{
+    return false;
+  }
 }
 
 function switchPages() {
   favPage.classList.toggle("hidden");
   mainPage.classList.toggle("hidden");
+  meditateImg.classList.remove("hidden");
+  messageBox.classList.add("hidden");
 }
 
 function addContent() {
   var message = null;
   showFavoriteMessage.innerHTML = "";
   for(var i = 0; i < favoritedMessages.length; i++){
-    message = `<p class="message-font">${favoritedMessages[i]}</p>`;
+    message = `<p class="message-font">${favoritedMessages[i]}<img class="favButton favPage" src="./assets/pink-heart.jpg"></p> `;
     showFavoriteMessage.innerHTML += message;
   }
 }
